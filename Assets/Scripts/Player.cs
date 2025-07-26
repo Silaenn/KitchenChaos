8965f3a26b215b75fcff0062857f047f257ad9f7
Silaenn <deokeldisilaen@gmail.com>
2025-07-26 17:40:04 +0700

    add input sistem refactor

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,32 +6,11 @@ using UnityEngine.AI;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 7f;
+    [SerializeField] GameInput gameInput;
     bool isWalking;
     void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
@@ -41,7 +20,6 @@ public class Player : MonoBehaviour
 
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-        Debug.Log(moveDir);
     }
 
     public bool IsWalking()
