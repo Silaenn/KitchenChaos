f24d4774fa181681f9e0c46c82831d74e61101a7
Silaenn <deokeldisilaen@gmail.com>
2025-08-08 00:56:07 +0700

    add mainmenu and loading

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] Button playButton;
+    [SerializeField] Button quitButton;
+
+    void Awake()
+    {
+        playButton.onClick.AddListener(() =>
+        {
+            Loader.Load(Loader.Scene.GameScene);
+        });
+
+        quitButton.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+    }
+
+}
